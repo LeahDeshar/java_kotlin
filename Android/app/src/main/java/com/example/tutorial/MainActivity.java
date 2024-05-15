754d71bbcdd4c562fe37cc6021026126fa5b3754
Leah Deshar <leah.desar@gmail.com>
2024-05-15 18:08:20 +0545

    6th sem exercise

--- Android/app/src/main/java/com/example/tutorial/MainActivity.java
+++ Android/app/src/main/java/com/example/tutorial/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tutorial;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        TextView txt1 = findViewById(R.id.textView2);
+        txt1.setText(R.string.test1
+        );
     }
 }
