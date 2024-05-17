913563c2f658feaa9363d37f56da40c156f26932
Leah Deshar <leah.desar@gmail.com>
2024-05-17 22:08:41 +0545

    6th sem unit-3

--- Android/app/src/main/java/com/example/tutorial/MainActivity2.java
+++ Android/app/src/main/java/com/example/tutorial/MainActivity2.java
@@ -1,14 +1,21 @@
 package com.example.tutorial;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -29,5 +36,31 @@ public class MainActivity2 extends AppCompatActivity {
         ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, course);
 
         spinner.setAdapter(adapter);
+
+
+        Button buttonDatePicker = findViewById(R.id.buttonDatePicker);
+
+        // Initialize DatePickerDialog with current date as default
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Do something with the selected date
+                        Toast.makeText(MainActivity2.this, "Selected Date: " + dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT).show();
+                    }
+                }, year, month, dayOfMonth);
+
+        buttonDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show DatePickerDialog when button is clicked
+                datePickerDialog.show();
+            }
+        });
     }
+
 }
