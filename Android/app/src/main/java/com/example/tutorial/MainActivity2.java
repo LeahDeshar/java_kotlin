7cb1f46fda07a81c6f4d916b35acd5f80faa9852
Leah Deshar <leah.desar@gmail.com>
2024-05-17 21:56:11 +0545

    6th sem unit-3

--- Android/app/src/main/java/com/example/tutorial/MainActivity2.java
+++ Android/app/src/main/java/com/example/tutorial/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.tutorial;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,12 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinner = findViewById(R.id.spinner);
+        String[] course = {"Java", "Python", "Kotlin", "C++", "C#"};
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, course);
+
+        spinner.setAdapter(adapter);
     }
 }
