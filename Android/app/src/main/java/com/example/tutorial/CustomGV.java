e52e32ad111433779a7f832c072ff77ec8c743a5
Leah Deshar <leah.desar@gmail.com>
2024-05-20 08:07:13 +0545

    6th sem unit-6 gridview

--- Android/app/src/main/java/com/example/tutorial/CustomGV.java
+++ Android/app/src/main/java/com/example/tutorial/CustomGV.java
@@ -1,12 +1,15 @@
 package com.example.tutorial;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class CustomGV extends AppCompatActivity {
 
@@ -20,5 +23,26 @@ public class CustomGV extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        GridView gridView = findViewById(R.id.gridView);
+
+        // Create an ArrayList and add sample data
+        ArrayList<Planet> planet = new ArrayList<>();
+        planet.add(new Planet("Mercury", "The smallest planet in our solar system", R.drawable.mercury));
+        planet.add(new Planet("Venus", "The second planet from the Sun", R.drawable.venus));
+        planet.add(new Planet("Earth", "Our home planet", R.drawable.earth));
+        planet.add(new Planet("Jupiter", "Jupiter", R.drawable.jupiter));
+        planet.add(new Planet("Saturn", "Saturn", R.drawable.saturn));
+        planet.add(new Planet("Uranus", "Uranus", R.drawable.uranus));
+
+
+        // Create a CustomGridViewAdapter
+        CustomGridViewAdapter adapter = new CustomGridViewAdapter(this, planet);
+
+        // Set the adapter to the GridView
+        gridView.setAdapter(adapter);
+
+
     }
 }
