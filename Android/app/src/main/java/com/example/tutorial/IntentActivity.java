08645b0e98e683d1ce0dfa1b979745771868af8c
Leah Deshar <leah.desar@gmail.com>
2024-05-21 09:37:48 +0545

    6th sem unit-4

--- Android/app/src/main/java/com/example/tutorial/IntentActivity.java
+++ Android/app/src/main/java/com/example/tutorial/IntentActivity.java
@@ -1,6 +1,11 @@
 package com.example.tutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class IntentActivity extends AppCompatActivity {
-
+TextView fname,lname,email;
+EditText etfname,etlname,etemail;
+Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,36 @@ public class IntentActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etfname = findViewById(R.id.editTextFName);
+        etlname = findViewById(R.id.editTextLName);
+        etemail = findViewById(R.id.editTextEmail);
+        submit = findViewById(R.id.button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getfname = etfname.getText().toString();
+                String getlname = etlname.getText().toString();
+                String getemail = etemail.getText().toString();
+
+                fname.setText(getfname);
+                lname.setText(getlname);
+                email.setText(getemail);
+
+
+
+
+
+
+//                Intent intent = new Intent(IntentActivity.this,IntentActivityII.class);
+//                intent.putExtra("fname",fname);
+//                intent.putExtra("lname",lname);
+//                intent.putExtra("email",email);
+//                startActivity(intent);
+            }
+        });
+
+
     }
 }
