ea9a33dfa31210ac2767c2193e2ac4de5c205eb8
Leah Deshar <leah.desar@gmail.com>
2024-05-29 22:10:37 +0545

    added crud

--- CRUD/app/src/main/java/com/example/crud/FetchDataActivity.java
+++ CRUD/app/src/main/java/com/example/crud/FetchDataActivity.java
@@ -9,8 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FetchDataActivity extends AppCompatActivity {
