195955de0e403169e184179349af7366c63a7fbd
Leah Deshar <leah.desar@gmail.com>
2024-06-12 16:28:36 +0545

    6th sem Assignment folder -main activity

--- Assignment/app/src/main/java/com/example/assignment/MainActivity.java
+++ Assignment/app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.assignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.assignment.Assignment1.AssignmentDetailsActivity;
+import com.example.assignment.Hello.HelloWorldActivity;
 
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] titles = {"Hello World", "Assignment:1", "Coming Soon"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String title = titles[position];
+                switch (title) {
+                    case "Hello World":
+                        startActivity(new Intent(MainActivity.this, HelloWorldActivity.class));
+                        break;
+                    case "Assignment:1":
+                        startActivity(new Intent(MainActivity.this, AssignmentDetailsActivity.class));
+                        break;
+                    default:
+                        // Handle default case
+                        break;
+                }
+            }
+            
         });
     }
 }
