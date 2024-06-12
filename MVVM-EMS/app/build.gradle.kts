29bfd7fe3427ebd4c133d28af8a7ad80ed367796
Leah Deshar <leah.desar@gmail.com>
2024-06-12 15:03:23 +0545

    MVVM -EMS Model

--- MVVM-EMS/app/build.gradle.kts
+++ MVVM-EMS/app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.room.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
