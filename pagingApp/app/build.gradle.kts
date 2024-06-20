2955a2e84ec353db05731100e3cc913de3cc28b3
Leah Deshar <leah.desar@gmail.com>
2024-06-20 15:16:58 +0545

    added Paging App

--- pagingApp/app/build.gradle.kts
+++ pagingApp/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
+//    alias(libs.plugins.hiltAndroid)
+//    id 'com.google.dagger.hilt.android' version '2.51.1'
 }
+//apply("com.android.application")
+//apply("com.google.dagger.hilt.android")
 
 android {
     namespace = "com.example.pagingapp"
@@ -29,9 +33,26 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        dataBinding = true
+    }
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+
+
+    val paging_version = "3.3.0"
+
+    implementation("androidx.paging:paging-runtime:$paging_version")
+// optional - RxJava3 support
+    implementation("androidx.paging:paging-rxjava3:$paging_version")
+
+
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.51.1")
 
     implementation(libs.appcompat)
     implementation(libs.material)
