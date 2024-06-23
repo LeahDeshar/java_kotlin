ae2e7d7171260bef469ed543d5da029a615da679
Leah Deshar <leah.desar@gmail.com>
2024-06-23 23:01:59 +0545

    added retrofit instance and repository

--- quizapp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.java
+++ quizapp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.java
@@ -1,4 +1,23 @@
 package com.example.quizapp.retrofit;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+
+    String baseURL = "http://127.0.0.1/quiz/";
+
+
+  /*  create and return a configured retrofit instance*/
+    public Retrofit getRetrofitInstance() {
+        return new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    /*create and return a configured QuestionAPI instance*/
+//    public QuestionAPI getQuestionAPI() {
+//        return getRetrofitInstance().create(QuestionAPI.class);
+//    }
 }
