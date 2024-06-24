f83f8adfe7bc5fac11bb548cfa286853af7d72d8
Leah Deshar <leah.desar@gmail.com>
2024-06-24 15:21:25 +0545

    main activity the quiz app

--- quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -8,10 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.quizapp.databinding.ActivityMainBinding;
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionList;
 
 import java.util.List;
 
@@ -45,5 +47,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+
+        DisplayFirstQuestion();
+    }
+    public void DisplayFirstQuestion(){
+        quizViewModel.getQuestionListLiveData().observe(this, new Observer<QuestionList>() {
+            @Override
+            public void onChanged(QuestionList questions) {
+                questionList = questions;
+                binding.txtQuestion.setText("Question 1: " +questions.get(0).getQuestion());
+                binding.radioButton1.setText(questions.get(0).getOption1());
+                binding.radioButton2.setText(questions.get(0).getOption2());
+                binding.radioButton3.setText(questions.get(0).getOption3());
+                binding.radioButton4.setText(questions.get(0).getOption4());
+//                binding.setMainActivity(MainActivity.this);
+            }
+        });
     }
 }
