dcdb1cf16cb351186bf3a3d3f98d9329127878aa
Leah Deshar <leah.desar@gmail.com>
2024-06-24 15:16:07 +0545

    main activity the quiz app

--- quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ quizapp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -7,8 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.quizapp.databinding.ActivityMainBinding;
+import com.example.quizapp.model.Question;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    QuizViewModel quizViewModel;
+    List<Question> questionList;
+
+
+
+    static int result = 0;
+    static int totalQuestions = 0;
+    int i =0 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        result =0;
+        totalQuestions = 0;
+
+
+        quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
     }
 }
