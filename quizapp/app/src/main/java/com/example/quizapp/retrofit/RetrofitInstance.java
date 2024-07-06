be76fd3cd71f0b15eb9c496ffc6cc22b65a4b446
Leah Deshar <leah.desar@gmail.com>
2024-07-06 10:51:45 +0545

    quizApp: bug fixed

--- quizapp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.java
+++ quizapp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.java
@@ -8,7 +8,7 @@ public class RetrofitInstance {
 //    String baseURL = "http://10.0.2.2/quiz/";
 
 
-    String baseURL = "http://127.0.0.1:3000/quiz/";
+    String baseURL = "http://192.168.1.4:3000/quiz/";
   /*  create and return a configured retrofit instance*/
     public Retrofit getRetrofitInstance() {
         return new Retrofit.Builder()
