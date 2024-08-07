0946cf74cd5c387251ee2729278285dad81e8187
Leah Deshar <leah.desar@gmail.com>
2024-08-07 07:56:41 +0545

    Firebase app: configuration

--- FirebaseApp/app/build.gradle.kts
+++ FirebaseApp/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +42,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+
+
+    implementation("com.google.firebase:firebase-analytics")
+
 }
