c0de85102c998b0497be8335e9f7a795a9bd84cd
Leah Deshar <leah.desar@gmail.com>
2024-09-16 11:13:38 +0545

    lotto app

--- kotlin/lotto/app/src/main/java/com/example/lotteryapp/LottoActivity.kt
+++ kotlin/lotto/app/src/main/java/com/example/lotteryapp/LottoActivity.kt
@@ -1,38 +1,20 @@
 package com.example.lotteryapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var nameTt: EditText
-    private lateinit var btn: Button
-
+class LottoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lotto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        nameTt = findViewById(R.id.editText)
-        btn = findViewById(R.id.btn)
-
-        btn.setOnClickListener{
-            val name:String = nameTt.text.toString()
-            val intent = Intent(this, LottoActivity::class.java)
-            intent.putExtra("name", name)
-            startActivity(intent)
-
         }
     }
 }
