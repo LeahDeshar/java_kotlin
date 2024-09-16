96a9a5ee5ad8d69a1aee813163bd114e5ba5d123
Leah Deshar <leah.desar@gmail.com>
2024-09-16 09:05:04 +0545

    Currency calculator with kotlin

--- currencyApp_Kotlin/app/src/main/java/com/example/currencyapp/MainActivity.kt
+++ currencyApp_Kotlin/app/src/main/java/com/example/currencyapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.currencyapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var resView: TextView
+    private lateinit var editText: EditText
+    private lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        resView = findViewById(R.id.resultView)
+        editText = findViewById(R.id.editAmount)
+        btn = findViewById(R.id.btnConvert)
+
+        btn.setOnClickListener{
+            val amount:Double = editText.text.toString().toDouble()
+            val result = amount * 0.94
+            resView.text = result.toString()
+
+        }
+
+
+
+
     }
 }
