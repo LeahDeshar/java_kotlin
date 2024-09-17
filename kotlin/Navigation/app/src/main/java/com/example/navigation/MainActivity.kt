58b6e98d3431b9e6f2bd1fe73dfd88cb40d68f0c
Leah Deshar <leah.desar@gmail.com>
2024-09-17 14:11:41 +0545

    nav bar

--- kotlin/Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
+++ kotlin/Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.navigation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var drawerLayout :DrawerLayout = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
