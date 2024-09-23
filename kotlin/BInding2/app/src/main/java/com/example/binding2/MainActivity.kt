b7044a634956bd5d4b0f9b2a212570bc786fce5b
Leah Deshar <leah.desar@gmail.com>
2024-09-23 14:22:59 +0545

    Binding data concept

--- kotlin/BInding2/app/src/main/java/com/example/binding2/MainActivity.kt
+++ kotlin/BInding2/app/src/main/java/com/example/binding2/MainActivity.kt
@@ -15,13 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 //        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.textView.text =  "databinding concept"
+
+        var user = User("sai", 25);
+        binding.user = user;
+
+//        binding.textView.text =  user.name + " " + user.age.toString();
     }
 }
---
+++ kotlin/BInding2/app/src/main/java/com/example/binding2/User.kt
@@ -0,0 +1,3 @@
+package com.example.binding2
+
+data class User(val name:String ,val age:Int  );
